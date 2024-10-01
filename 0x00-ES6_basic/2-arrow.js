762302bb75cab33b128795ab76b22eb2ebf96334
Ngayep <courtesyofjess@gmail.com>
2024-10-01 22:20:11 +0100

    2-arrow.js

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,9 +1,10 @@
 export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-  const self = this;
-  this.addNeighborhood = add(newNeighborhood) => {
-    this.sanFranciscoNeighborhoods.push(newNeighborhood);
-    return this.sanFranciscoNeighborhoods;
+  
+const self = this;
+this.addNeighborhood = add(newNeighborhood) => {
+	self.sanFranciscoNeighborhoods.push(newNeighborhood);
+		return this.sanFranciscoNeighborhoods;
   };
 }
